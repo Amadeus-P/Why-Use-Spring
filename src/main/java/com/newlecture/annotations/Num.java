2414db6c2c5bd4d48a8b422a648e9caffd375389
Amadeus-P <halin1885@gmail.com>
2024-08-02 14:05:46 +0900

    빈 프로젝트(오류예상)

--- src/main/java/com/newlecture/annotations/Num.java
+++
@@ -1,9 +0,0 @@
-package com.newlecture.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Num {
-
-}
